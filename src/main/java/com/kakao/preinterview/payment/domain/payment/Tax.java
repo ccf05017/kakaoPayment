1677aaa70ead924fa00ec5f813bd101297f3ad13
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-13 23:14:20 +0900

    feat(Tax): equals, autoCreate
    - 동등 비교가 가능하도록 구현
    - 결제 금액을 입력받아서 자동으로 부가가치세를 계산하는 메서드 구현
    - null로 자동 생성 불가
    - 0이하의 값으로 자동 생성 불가

--- src/main/java/com/kakao/preinterview/payment/domain/payment/Tax.java
+++ src/main/java/com/kakao/preinterview/payment/domain/payment/Tax.java
@@ -1,6 +1,7 @@
 package com.kakao.preinterview.payment.domain.payment;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Tax {
     private BigDecimal value;
@@ -15,7 +16,31 @@ public class Tax {
     }
 
     private static void manualValidation(BigDecimal value, BigDecimal payAmount) {
-        if (BigDecimal.ZERO.compareTo(value) > 0 || payAmount.compareTo(value) < 0)
-            throw new IllegalArgumentException();
+        if (value == null || payAmount == null) throw new IllegalArgumentException();
+        if (BigDecimal.ZERO.compareTo(value) > 0) throw new IllegalArgumentException();
+        if (payAmount.compareTo(value) < 0) throw new IllegalArgumentException();
+    }
+
+    public static BigDecimal autoCreate(BigDecimal payAmount) {
+        autoValidation(payAmount);
+        return payAmount.divide(BigDecimal.valueOf(11), BigDecimal.ROUND_HALF_UP);
+    }
+
+    private static void autoValidation(BigDecimal payAmount) {
+        if (payAmount == null) throw new IllegalArgumentException();
+        if (BigDecimal.ZERO.compareTo(payAmount) >= 0) throw new IllegalArgumentException();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tax tax = (Tax) o;
+        return Objects.equals(value, tax.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
