07492e747594d086abe0b3fc30e3321c1d16cbe5
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-13 22:10:41 +0900

    test(PayInfoTest): merge
    - validation 함수의 경우를 합쳐서 볼 수 있도록 테스트 케이스 병합

--- src/test/java/com/kakao/preinterview/payment/domain/payment/PayInfoTests.java
+++ src/test/java/com/kakao/preinterview/payment/domain/payment/PayInfoTests.java
@@ -3,8 +3,8 @@ package com.kakao.preinterview.payment.domain.payment;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.math.BigDecimal;
 import java.util.stream.Stream;
@@ -23,26 +23,23 @@ class PayInfoTests {
         assertThat(PayInfo.create(installmentMonths, payAmount, payStatus)).isNotNull();
     }
 
-    @DisplayName("0 ~ 12의 범위를 벗어난 할부개월수로는 객체를 생성할 수 없다.")
+    @DisplayName("올바르지 못한 인자로 객체를 생성할 수 없다.")
     @ParameterizedTest
-    @ValueSource(ints = {-1, 13})
-    void createFailByInvalidInstallmentMonths(int invalidInstallmentMonths) {
-        assertThatThrownBy(() -> PayInfo.create(invalidInstallmentMonths, payAmount, payStatus))
+    @MethodSource("invalidParams")
+    void validationTest(int invalidInstallmentMonths, BigDecimal invalidPayAmount) {
+        assertThatThrownBy(() -> PayInfo.create(invalidInstallmentMonths, invalidPayAmount, payStatus))
                 .isInstanceOf(IllegalArgumentException.class);
     }
-
-    @DisplayName("100원 미만이나 10억 초과의 결제 금액으로는 객체를 생성할 수 없다.")
-    @ParameterizedTest
-    @MethodSource("invalidPayAmount")
-    void createFailByInvalidPayAmount(BigDecimal invalidPayAmount) {
-        assertThatThrownBy(() -> PayInfo.create(installmentMonths, invalidPayAmount, payStatus))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-    public static Stream<BigDecimal> invalidPayAmount() {
+    public static Stream<Arguments> invalidParams() {
         return Stream.of(
-                BigDecimal.valueOf(99),
-                BigDecimal.valueOf(1000000001),
-                BigDecimal.valueOf(100000000000L)
+                // invalid installmentMonths
+                Arguments.of(-1, BigDecimal.valueOf(1000)),
+                Arguments.of(13, BigDecimal.valueOf(1000)),
+
+                // invalid payAmount
+                Arguments.of(12, BigDecimal.valueOf(99)),
+                Arguments.of(12, BigDecimal.valueOf(1000000001)),
+                Arguments.of(12, BigDecimal.valueOf(100000000000L))
         );
     }
 }
