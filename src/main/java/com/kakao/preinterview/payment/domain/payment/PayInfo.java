e7b340bb2aabdfdafd13b6e929cd11bdcd1be940
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-13 22:06:09 +0900

    feat(PayInfo): validation
    - 할부개월수, 결제금액, 결제타입에 대한 validation 구현
    - 할부개월수는 0 ~ 12 사이의 정수
    - 결제금액은 100 ~ 10억 사이의 BigDecimal
    - 결제 타입은 enum 상수

--- src/main/java/com/kakao/preinterview/payment/domain/payment/PayInfo.java
+++ src/main/java/com/kakao/preinterview/payment/domain/payment/PayInfo.java
@@ -3,14 +3,36 @@ package com.kakao.preinterview.payment.domain.payment;
 import java.math.BigDecimal;
 
 public class PayInfo {
+    private final int MIN_INSTALLMENT_MONTHS = 0;
+    private final int MAX_INSTALLMENT_MONTHS = 12;
+    private final BigDecimal MIN_PAY_AMOUNT = BigDecimal.valueOf(100);
+    private final BigDecimal MAX_PAY_AMOUNT = BigDecimal.valueOf(1000000000);
+
     private Integer installmentMonths;
     private BigDecimal payAmount;
     private PayStatus payStatus;
 
     private PayInfo(int installmentMonths, BigDecimal payAmount, PayStatus payStatus) {
+        validation(installmentMonths, payAmount, payStatus);
+
         this.installmentMonths = installmentMonths;
         this.payAmount = payAmount;
         this.payStatus = payStatus;
+    }
+
+    private void validation(int installmentMonths, BigDecimal payAmount, PayStatus payStatus) {
+        installmentMonthsValidation(installmentMonths);
+        payAmountValidation(payAmount);
+    }
+
+    private void installmentMonthsValidation(int installmentMonths) {
+        if (installmentMonths < MIN_INSTALLMENT_MONTHS || installmentMonths > MAX_INSTALLMENT_MONTHS)
+            throw new IllegalArgumentException("Invalid InstallmentMonths");
+    }
+
+    private void payAmountValidation(BigDecimal payAmount) {
+        if (MIN_PAY_AMOUNT.compareTo(payAmount) > 0 || MAX_PAY_AMOUNT.compareTo(payAmount) < 0)
+            throw new IllegalArgumentException("Invalid PayAmount");
     }
 
     public static PayInfo create(int installmentMonths, BigDecimal payAmount, PayStatus payStatus) {
--- src/test/java/com/kakao/preinterview/payment/domain/payment/PayInfoTests.java
+++ src/test/java/com/kakao/preinterview/payment/domain/payment/PayInfoTests.java
@@ -2,10 +2,15 @@ package com.kakao.preinterview.payment.domain.payment;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.math.BigDecimal;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PayInfoTests {
     private int installmentMonths = 12;
@@ -16,5 +21,28 @@ class PayInfoTests {
     @Test
     void createTest() {
         assertThat(PayInfo.create(installmentMonths, payAmount, payStatus)).isNotNull();
+    }
+
+    @DisplayName("0 ~ 12의 범위를 벗어난 할부개월수로는 객체를 생성할 수 없다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 13})
+    void createFailByInvalidInstallmentMonths(int invalidInstallmentMonths) {
+        assertThatThrownBy(() -> PayInfo.create(invalidInstallmentMonths, payAmount, payStatus))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("100원 미만이나 10억 초과의 결제 금액으로는 객체를 생성할 수 없다.")
+    @ParameterizedTest
+    @MethodSource("invalidPayAmount")
+    void createFailByInvalidPayAmount(BigDecimal invalidPayAmount) {
+        assertThatThrownBy(() -> PayInfo.create(installmentMonths, invalidPayAmount, payStatus))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    public static Stream<BigDecimal> invalidPayAmount() {
+        return Stream.of(
+                BigDecimal.valueOf(99),
+                BigDecimal.valueOf(1000000001),
+                BigDecimal.valueOf(100000000000L)
+        );
     }
 }
