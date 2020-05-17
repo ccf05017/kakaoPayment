934c3bbbc0c6090d55b0ed82603498fada91a695
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-17 14:42:03 +0900

    refactor(GetPayHistoryResponseDto)
    - 도메인 객체를 주입받아 스스로 만들 수 있도록 리팩토링

--- src/main/java/com/kakao/preinterview/payment/application/PaymentHistoryService.java
+++ src/main/java/com/kakao/preinterview/payment/application/PaymentHistoryService.java
@@ -5,7 +5,6 @@ import com.kakao.preinterview.payment.domain.history.PaymentHistory;
 import com.kakao.preinterview.payment.domain.history.PaymentHistoryRepository;
 import com.kakao.preinterview.payment.domain.payment.CardInfo;
 import com.kakao.preinterview.payment.domain.service.DecryptService;
-import com.kakao.preinterview.payment.ui.dto.CardInfoData;
 import com.kakao.preinterview.payment.ui.dto.GetPayHistoryResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,27 +22,6 @@ public class PaymentHistoryService {
                 .orElseThrow(NotExistPaymentHistoryException::new);
         CardInfo cardInfo = decryptService.getCardInfoFromPaymentHistory(paymentHistory);
 
-        return GetPayHistoryResponseDto.builder()
-                .managementNumber(paymentHistory.getManagementNumber())
-                .status(paymentHistory.getPaymentStatusName())
-                .payAmount(paymentHistory.getPayAmount())
-                .taxAmount(paymentHistory.getTax())
-                .cardInfoData(CardInfoData.builder()
-                        .cardNumber(cardNumberBlocker(cardInfo.getCardNumber().toString()))
-                        .duration(cardInfo.getDuration())
-                        .cvc(cardInfo.getCvc())
-                        .build())
-                .build();
-    }
-
-    private String cardNumberBlocker(String cardNumberString) {
-        StringBuilder starBlocks = new StringBuilder();
-        for (int i = 0; i < cardNumberString.length() - 9; i ++) {
-            starBlocks.append("*");
-        }
-
-        return cardNumberString.substring(0, 6)
-                + starBlocks.toString()
-                + cardNumberString.substring(cardNumberString.length() - 3);
+        return GetPayHistoryResponseDto.create(paymentHistory, cardInfo);
     }
 }
--- src/main/java/com/kakao/preinterview/payment/ui/dto/GetPayHistoryResponseDto.java
+++ src/main/java/com/kakao/preinterview/payment/ui/dto/GetPayHistoryResponseDto.java
@@ -1,5 +1,7 @@
 package com.kakao.preinterview.payment.ui.dto;
 
+import com.kakao.preinterview.payment.domain.history.PaymentHistory;
+import com.kakao.preinterview.payment.domain.payment.CardInfo;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,4 +19,29 @@ public class GetPayHistoryResponseDto {
     private String status;
     private BigDecimal payAmount;
     private BigDecimal taxAmount;
+
+    public static GetPayHistoryResponseDto create(PaymentHistory paymentHistory, CardInfo cardInfo) {
+        return GetPayHistoryResponseDto.builder()
+                .managementNumber(paymentHistory.getManagementNumber())
+                .status(paymentHistory.getPaymentStatusName())
+                .payAmount(paymentHistory.getPayAmount())
+                .taxAmount(paymentHistory.getTax())
+                .cardInfoData(CardInfoData.builder()
+                        .cardNumber(cardNumberBlocker(cardInfo.getCardNumber().toString()))
+                        .duration(cardInfo.getDuration())
+                        .cvc(cardInfo.getCvc())
+                        .build())
+                .build();
+    }
+
+    private static String cardNumberBlocker(String cardNumberString) {
+        StringBuilder starBlocks = new StringBuilder();
+        for (int i = 0; i < cardNumberString.length() - 9; i ++) {
+            starBlocks.append("*");
+        }
+
+        return cardNumberString.substring(0, 6)
+                + starBlocks.toString()
+                + cardNumberString.substring(cardNumberString.length() - 3);
+    }
 }
